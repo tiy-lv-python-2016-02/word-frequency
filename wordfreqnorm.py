6e3c85fd2369a976a5d4e08761a805f375df34cc
Kevin Mullen <kevinkozzik@gmail.com>
2016-03-02 16:36:53 -0800

    normal mode without comments

--- wordfreqnorm.py
+++ wordfreqnorm.py
@@ -1,17 +1,6 @@
 from re import sub
 from collections import Counter
 
-
-# def word_counter(text):
-# freq = {}
-# frequency is an empty dictionary
-# going to be storing words as the key
-# and number of times they pop up by their value
-
-
-# def word_counter(text):
-    # freq = {}
-
 if __name__ == '__main__':
     with open("sample.txt") as file:
         lines = file.readlines()
@@ -25,19 +14,8 @@ if __name__ == '__main__':
             words_from_text.append(word)
 
     freq = Counter(words_from_text)
+
     top_words = freq.most_common(20)
+
     for key, value in top_words:
         print("{}: {}".format(key, value))
-
-# s = re.sub('[^0-9a-zA-Z]+', '*', s)
-# uses the sub function from the re library
-# won't need re because we imported sub itself from re
-# all non characters will be replaced with second argument
-# on the third argument, your string
-# the result of this becomes your new variable (s in this case)
-
-
-# Your program should open sample.txt and read in the entirety of its text.
-# You'll need to normalize the text so that words in different cases are
-# still the same word and so it's scrubbed of punctuation.
-# Once you've done that, go through the text and find the number of times each word is used.
